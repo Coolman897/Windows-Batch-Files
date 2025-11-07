4897a2d84cb291be5a16f49d35b06e4a2c9508de
Coolman897 <coolman897@protonmail.com>
2025-11-07 10:13:35 -0500

    Update Ultimate-System-Maintenance.bat

--- Ultimate-System-Maintenance.bat
+++ Ultimate-System-Maintenance.bat
@@ -2,18 +2,6 @@
 title Ultimate Windows Maintenance Script (Full Maintenance - Run as Administrator)
 cls
 
-REM =========================================================
-REM --- DIGITAL DEFENDER'S SCRIPT SIGNATURE ---
-REM =========================================================
-REM PROJECT: OS Optimization and Security Suite
-REM AUTHOR: [Coolman]
-REM MISSION: Maintaining a fast, clean, and private Windows environment.
-REM CREATION DATE: 2025-11-07
-REM LAST MODIFIED: 2025-11-07
-REM NOTE: Always run this script as Administrator for full system access.
-REM =========================================================
-
-
 echo =========================================================
 echo  STARTING FULL SYSTEM DIAGNOSTIC AND MAINTENANCE ROUTINE
 echo =========================================================
@@ -64,3 +52,4 @@ echo Exiting Maintenance Tool. Thank you!
 timeout /t 2 /nobreak >nul
 
 exit
+
